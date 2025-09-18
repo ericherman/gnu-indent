b5034e806e37c6762ed8f1a1ccb20a92734796d7
Eric Herman <eric@freesa.org>
2025-09-18 11:55:51 +0200

    Add regression test for dont indent extern "C"
    This adds a test similar to the one described by Mike Frysinger
    in https://savannah.gnu.org/bugs/?65165
    
    Signed-off-by: Eric Herman <eric@freesa.org>

---
+++ regression/input/dont-indent-after-extern-c.c
@@ -0,0 +1,9 @@
+#if defined __cplusplus
+extern "C" {
+#endif
+
+extern int foo (void);
+
+#if defined __cplusplus
+}
+#endif
---
+++ regression/standard/dont-indent-after-extern-c.c
@@ -0,0 +1,10 @@
+#if defined __cplusplus
+extern "C"
+{
+#endif
+
+extern int foo (void);
+
+#if defined __cplusplus
+}
+#endif
